65a52bcb68e94d9f945415f1b4eb94abd642a9c5
DuyAnh <anhduynguyencao@gmail.com>
2024-05-21 11:59:13 +0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Cấu hình basePath nếu ứng dụng của bạn được triển khai dưới một basePath cụ thể
+  basePath: "/home",
 
-export default nextConfig;
+  // Cấu hình tên miền và path prefix cho ứng dụng của bạn
+  // assetPrefix: '/myapp/',
+
+  // Cấu hình định dạng tệp và thư mục cho các tệp ảnh, font chứa trong ứng dụng
+  images: {
+    domains: ["example.com"],
+  },
+
+  // Cấu hình environment variables
+  env: {
+    API_URL: process.env.API_URL,
+  },
+
+  // Cấu hình Webpack hoặc các plugins khác
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Thực hiện các thay đổi vào cấu hình webpack ở đây
+    return config;
+  },
+
+  // Cấu hình các headers cho trang
+  headers: async () => {
+    return [
+      {
+        // Cache HTML trong 10 giây
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=10, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
